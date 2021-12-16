c54f55227d1d7af4ec4871db9480709af04fdb59
Vladyslav Radchuk <cepishmohnoril@gmail.com>
2021-12-16 14:06:28 +0200

    simple default event listener

---
+++ theory/src/Services/KernelResponseListener.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
+class KernelResponseListener {
+
+    public function onKernelResponse(ResponseEvent $event) {
+        $response = new Response('Doot');
+        $event->setResponse($response);
+    }
+}
