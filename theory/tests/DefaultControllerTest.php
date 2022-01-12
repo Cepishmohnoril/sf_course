05ff06b07949a2189b79e061baa4cc9f96e00e9e
Vladyslav Radchuk <cepishmohnoril@gmail.com>
2022-01-12 14:15:19 +0200

    playing around with tests setup

--- theory/tests/DefaultControllerTest.php
+++ theory/tests/DefaultControllerTest.php
@@ -6,10 +6,39 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+
+    private $em;
+    private $client;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (empty($this->client)) {
+            $this->client = static::createClient();
+        }
+
+        $this->em = $this->client->getContainer()->get('doctrine.orm.entity_manager');
+        $this->em->beginTransaction();
+        $this->em->getConnection()->setAutoCommit(false);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        //$this->em->rollback(); // Generates error for some reason
+
+        //It seems in Symfony5 trick with transaction is done by separate bundle
+        //https://symfony.com/doc/current/testing.html#resetting-the-database-automatically-before-each-test
+
+        $this->em->close();
+        $this->em = null;
+    }
+
     public function testParam(): void
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/param/doot');
+        $crawler = $this->client->request('GET', '/param/doot');
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('body', 'Hello doot');
@@ -17,27 +46,25 @@ class DefaultControllerTest extends WebTestCase
 
     public function testLink(): void
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', 'link');
+        $crawler = $this->client->request('GET', 'link');
 
         $this->assertResponseIsSuccessful();
         $link = $crawler->filter('body > a:contains("Login")')->link();
 
-        $crawler = $client->click($link);
+        $crawler = $this->client->click($link);
         $this->assertTrue((bool) $crawler->filter('label:contains("Keep me logged in")')->count());
     }
 
     ///public function testLogin(): void
     ///{
-    ///    $client = static::createClient();
-    ///    $crawler = $client->request('GET', 'login');
+    ///    $crawler = $this->client->request('GET', 'login');
     ///
     ///    $form = $crawler->selectButton('login')->form();
     ///    $form['email'] = 'cepishmohnoril+1@gmail.com';
     ///    $form['password'] = '123qweqwe';
-    ///    $crawler = $client->submit($form);
+    ///    $crawler = $this->client->submit($form);
     ///
-    ///    $crawler = $client->followRedirect();
+    ///    $crawler = $this->client->followRedirect();
     ///
     ///    $this->assertTrue((bool) $crawler->filter('a:contains("Logout")')->count());
     ///}
@@ -47,8 +74,7 @@ class DefaultControllerTest extends WebTestCase
      */
     public function testRoute($url): void
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', $url);
+        $crawler = $this->client->request('GET', $url);
         $this->assertResponseIsSuccessful();
     }
 
