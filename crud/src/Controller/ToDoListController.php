97f568297cd8040ed65f54e7ce4aceb5fadb6000
Vladyslav Radchuk <cepishmohnoril@gmail.com>
2022-01-19 20:08:23 +0200

    crud lessons are finished

--- crud/src/Controller/ToDoListController.php
+++ crud/src/Controller/ToDoListController.php
@@ -2,17 +2,65 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ToDoListController extends AbstractController
 {
     /**
-     * @Route("/to/do/list", name="to_do_list")
+     * @Route("/", name="list_task")
      */
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
-        return $this->render('index.html.twig');
+        $tasks = $doctrine->getRepository(Task::class)->findBy([], ['id' => 'DESC']);
+
+        return $this->render('index.html.twig', ['tasks' => $tasks]);
+    }
+
+    /**
+     * @Route("/create", name="create_task", methods={"POST"})
+     */
+    public function create(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $title = trim($request->request->get('title'));
+
+        if (!empty($title)) {
+            $em = $doctrine->getManager();
+            $task = new Task();
+            $task->setTitle($title);
+            $em->persist($task);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('list_task');
+    }
+
+    /**
+     * @Route("/switch-status/{id}", name="switch_status")
+     */
+    public function switchStatus(int $id, ManagerRegistry $doctrine): Response
+    {
+        $em = $doctrine->getManager();
+        $task = $doctrine->getRepository(Task::class)->find($id);
+        $task->setStatus(!$task->getStatus());
+        $em->flush();
+
+        return $this->redirectToRoute('list_task');
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete_task")
+     */
+    public function delete(Task $task, ManagerRegistry $doctrine): Response
+    {
+        $em = $doctrine->getManager();
+        $em->remove($task);
+        $em->flush();
+
+        return $this->redirectToRoute('list_task');
     }
 }
