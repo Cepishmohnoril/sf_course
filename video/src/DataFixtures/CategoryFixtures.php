0eaa61582c25dde5375de30ef0bf47e48d44da79
Vladyslav Radchuk <cepishmohnoril@gmail.com>
2022-03-17 17:58:05 +0200

    more fixtures for subcategories

--- video/src/DataFixtures/CategoryFixtures.php
+++ video/src/DataFixtures/CategoryFixtures.php
@@ -12,6 +12,11 @@ class CategoryFixtures extends Fixture
     {
         $this->loadMainCategories($manager);
         $this->loadSubcategories($manager, 'Electronics', 1);
+        $this->loadSubcategories($manager, 'Computers', 6);
+        $this->loadSubcategories($manager, 'Laptops', 8);
+        $this->loadSubcategories($manager, 'Books', 3);
+        $this->loadSubcategories($manager, 'Movies', 4);
+        $this->loadSubcategories($manager, 'Romance', 18);
     }
 
     private function loadMainCategories($manager): void
@@ -56,6 +61,51 @@ class CategoryFixtures extends Fixture
             ['Cameras', 5],
             ['Computers', 6],
             ['Cell Phones', 7],
+        ];
+    }
+
+    private function getComputersData(): array
+    {
+        return [
+            ['Laptops', 8],
+            ['Desktops', 9],
+        ];
+    }
+
+    private function getLaptopsData()
+    {
+        return [
+
+            ['Apple',10],
+            ['Asus',11],
+            ['Dell',12],
+            ['Lenovo',13],
+            ['HP',14]
+
+        ];
+    }
+
+    private function getBooksData()
+    {
+        return [
+            ['Children\'s Books',15],
+            ['Kindle eBooks',16],
+        ];
+    }
+
+    private function getMoviesData()
+    {
+        return [
+            ['Family',17],
+            ['Romance',18],
+        ];
+    }
+
+    private function getRomanceData()
+    {
+        return [
+            ['Romantic Comedy',19],
+            ['Romantic Drama',20],
         ];
     }
 }
