2231133477d2f4694114c6ac7ef767f6a3fa710d
Vladyslav Radchuk <cepishmohnoril@gmail.com>
2022-04-20 20:32:36 +0300

    some bullshit, object in Twig is interesting, but does it necessery?

--- video/src/Controller/FrontController.php
+++ video/src/Controller/FrontController.php
@@ -24,10 +24,9 @@ class FrontController extends AbstractController
      */
     public function videoList($id, CategoryTreeFrontPage $categories): Response
     {
-        $subcategories = $categories->buildTree($id);
-
+        $categories->getCategoryListAndParent($id);
         return $this->render('front/video_list.html.twig', [
-            'subcategories' => $categories->getCategoryList($subcategories),
+            'subcategories' => $categories,
         ]);
     }
 
--- video/src/Utils/CategoryTreeFrontPage.php
+++ video/src/Utils/CategoryTreeFrontPage.php
@@ -1,29 +1,60 @@
 <?php
 
 namespace App\Utils;
+use App\Twig\AppExtension;
 
 use App\Utils\AbstractClasses\CategoryTreeAbstract;
 
+//Some bullshit, but it is in the lesson
 class CategoryTreeFrontPage extends CategoryTreeAbstract
 {
+    public function getCategoryListAndParent(int $id): string
+    {
+        $this->slugger = new AppExtension; // Twig extension to slugify url's for categories
+        $parentData = $this->getMainParent($id); // main parent of subcategory
+        $this->mainParentName = $parentData['name']; // for accesing in view
+        $this->mainParentId = $parentData['id']; // for accesing in view
+        $key = array_search($id, array_column($this->categories,'id'));
+        $this->currentCategoryName = $this->categories[$key]['name']; // for accesing in view
+        $categories_array = $this->buildTree($parentData['id']); // builds array for generating nested html list
+        return $this->getCategoryList($categories_array);
+    }
+
     public function getCategoryList(array $categories)
     {
-        //TODOOT
         $this->categoryList .= '<ul>';
 
         foreach($categories as $value) {
-            $catName = $value['name'];
+            $catName = $this->slugger->slugify($value['name']);
             $url = $this->urlGen->generate('video_list', ['categoryName' => $catName, 'id' => $value['id']]);
 
-            $this->categoryList .= "<li><a href=\"#\">$catName</a>";
+            $this->categoryList .= "<li><a href=\"$url\">$catName</a>";
 
             if (!empty($value['children'])) {
                 $this->getCategoryList($value['children']);
             }
+
+            $this->categoryList .= "</li>";
         }
 
         $this->categoryList .= '</ul>';
 
         return $this->categoryList;
+    }
+
+    public function getMainParent(int $id): array
+    {
+        $key = array_search($id, array_column($this->categories, 'id'));
+        if($this->categories[$key]['parent_id'] != null)
+        {
+            return $this->getMainParent($this->categories[$key]['parent_id']);
+        }
+        else
+        {
+            return [
+                'id'=>$this->categories[$key]['id'],
+                'name'=>$this->categories[$key]['name']
+                ];
+        }
     }
 }
